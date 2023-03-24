7ede1eb2e8fdc813cb3c8aba298bbf83cf9bcf9a
Alexandr707 <alexandralennikov90@gmail.com>
2023-03-24 19:37:47 +0700

    add Review REST-api

--- amazon-back/src/review/review.controller.ts
+++ amazon-back/src/review/review.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { ReviewDto } from './dto/review.dto';
 import { ReviewService } from './review.service';
 
-@Controller('review')
+@Controller('reviews')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
+
+  @UsePipes(new ValidationPipe())
+  @Get(':productId')
+  async getAll(@Param('productId') productId: string) {
+    return this.reviewService.getAll(+productId);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('leave/:productId')
+  @Auth()
+  async leaveReview(
+    @CurrentUser('id') id: number,
+    @Body() dto: ReviewDto,
+    @Param('productId') productId: string,
+  ) {
+    return this.reviewService.create(id, dto, +productId);
+  }
 }
--- amazon-back/src/review/review.service.ts
+++ amazon-back/src/review/review.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { ReviewDto } from './dto/review.dto';
+import { returnReviewObject } from './return-review.object';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAll(productId: number) {
+    return await this.prisma.review.findMany({
+      where: {
+        productId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      select: returnReviewObject,
+    });
+  }
+
+  async create(userId: number, dto: ReviewDto, productId: number) {
+    return this.prisma.review.create({
+      data: {
+        ...dto,
+        product: {
+          connect: {
+            id: productId,
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+
+  async getAverageByProductId(productId: number) {
+    return this.prisma.review
+      .aggregate({
+        where: { productId },
+        _avg: { rating: true },
+      })
+      .then(data => data._avg);
+  }
+}
